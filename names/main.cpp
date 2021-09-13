00a3b118c2dc8f5021f0232ca97f491a7fe39237
Antony Orlovsky <orlovskiy@niisi.ru>
2021-09-13 22:15:56 +0300

    Updated names

--- names/main.cpp
+++ names/main.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 
 using namespace std;
+
+string FindForYear(int year, const map<int, string>& names) {
+    string result;
+
+    for (const auto& item : names) {
+        if (item.first <= year)
+            result = item.second;
+        else
+            break;
+    }
+
+    return result;
+}
+
+vector<string> FindForYearWithHistory(int year, const map<int, string>& names) {
+    vector<string> result;
+
+    string prev_name = "";
+
+    for (const auto& item : names) {
+        if (item.first <= year) {
+            if (prev_name != item.second) {
+                result.push_back(item.second);
+                prev_name = item.second;
+            }
+        }
+        else {
+            break;
+        }
+    }
+
+    return result;
+}
+
+string GetHistory(const vector<string>& history) {
+    int history_length = history.size();
+    string result = history[history_length - 1];
+
+    if (history_length == 1)
+        return result;
+
+    result += " (";
+
+    for (int i = history_length - 2; i >= 0; --i) {
+        result += history[i];
+        result += i > 0 ? ", " : ")";
+    }
+
+    return result;
+}
 
 class Person {
 public:
@@ -13,32 +64,40 @@ public:
         surname_history[year] = last_name;
     }
     string GetFullName(int year) {
-        string name = "Incognito";
+        string name = FindForYear(year, name_history);
+        string surname = FindForYear(year, surname_history);
 
-        for (const auto& item : name_history) {
-            if (item.first > year)
-                break;
-            else
-                name = item.second;
-        }
+        bool has_name = !name.empty();
+        bool has_surname = !surname.empty();
 
-        string surname = "Incognito";
-
-        for (const auto& item : surname_history) {
-            if (item.first > year)
-                break;
-            else
-                surname = item.second;
-        }
-
-        if (name != "Incognito") {
+        if (has_name) {
             string result = name + ' ';
-            result += (surname != "Incognito") ? surname : "with unknown last name";
+            result += has_surname ? surname : "with unknown last name";
 
             return result;
         }
-        else if (surname != "Incognito") {
+        else if (has_surname) {
             return surname + " with unknown first name";
+        }
+        else {
+            return "Incognito";
+        }
+    }
+    string GetFullNameWithHistory(int year) {
+        vector<string> names = FindForYearWithHistory(year, name_history);
+        vector<string> surnames = FindForYearWithHistory(year, surname_history);
+
+        bool has_names = !names.empty();
+        bool has_surnames = !surnames.empty();
+
+        if (has_names) {
+            string result = GetHistory(names) + ' ';
+            result += has_surnames ? GetHistory(surnames) : "with unknown last name";
+
+            return result;
+        }
+        else if (has_surnames) {
+            return GetHistory(surnames) + " with unknown first name";
         }
         else {
             return "Incognito";
@@ -48,26 +107,3 @@ private:
     map<int, string> name_history;
     map<int, string> surname_history;
 };
-
-int main() {
-    Person person;
-
-    person.ChangeFirstName(1965, "Polina");
-    person.ChangeLastName(1967, "Sergeeva");
-    for (int year : {1900, 1965, 1990}) {
-        cout << person.GetFullName(year) << endl;
-    }
-
-    person.ChangeFirstName(1970, "Appolinaria");
-    for (int year : {1969, 1970}) {
-        cout << person.GetFullName(year) << endl;
-    }
-
-    person.ChangeLastName(1968, "Volkova");
-    for (int year : {1969, 1970}) {
-        cout << person.GetFullName(year) << endl;
-    }
-
-    return 0;
-}
-
